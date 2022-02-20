d0049f42eed7d50b57dc031fac688ecd6df3f467
alan <alancitus@gmail.com>
2022-02-19 23:47:31 -0500

    updated service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@nestjs/common';
-
+import { HttpService } from '@nestjs/axios';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class AppService {
+
+  constructor(private httpService: HttpService) { }
+
   getUser(params) {
-    return 'Hello World!';
+    const clientId = "Iv1.5c5906816666c600";
+    const clientSecret = "1d29344733dad46077234442f8177523d739113c";
+
+    return this.httpService
+      .get(`http://api.github.com/repos/${params.username}/apigithub/commits?client_id=${clientId}&client_secret=${clientSecret}`)
+      .pipe(
+        map((response) => response.data),
+        map((data) => ({
+          datos: data
+        }))
+      );
   }
 }
